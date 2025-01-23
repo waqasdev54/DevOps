f706c00e5ac8eeb297b3d98cb2e56c8ba6a804a3
Waqas Ahmed <108932019+waqasdev54@users.noreply.github.com>
2025-01-24 00:36:16 +0500

    added github api calls to restrict users to create repos

---
+++ Jenkins/repo-creation-policies.groovy
@@ -0,0 +1,46 @@
+def GITHUB_API_URL = 'https://api.github.com'
+def GITHUB_ORG = 'Xoftlabs'
+
+pipeline {
+    agent any
+    
+    stages {
+        stage('Disable Repository Creation Privileges') {
+            steps {
+                withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+                    script {
+                        // Update organization members repository creation settings
+                        def payload = groovy.json.JsonOutput.toJson([
+                            members_can_create_public_repositories: false,
+                            members_can_create_private_repositories: false
+                        ])
+
+                        def response = sh(
+                            script: """
+                                curl -L \
+                                -X PATCH \
+                                -H "Accept: application/vnd.github+json" \
+                                -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+                                -H "X-GitHub-Api-Version: 2022-11-28" \
+                                -H "Content-Type: application/json" \
+                                https://api.github.com/orgs/${GITHUB_ORG} \
+                                -d '${payload}'
+                            """,
+                            returnStdout: true
+                        ).trim()
+                        
+                        def responseData = readJSON(text: response)
+                        
+                        // Validate settings were updated
+                        if (responseData.members_can_create_public_repositories == false && 
+                            responseData.members_can_create_private_repositories == false) {
+                            echo "Successfully disabled repository creation for all organization members"
+                        } else {
+                            error "Failed to disable repository creation privileges: ${response}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
