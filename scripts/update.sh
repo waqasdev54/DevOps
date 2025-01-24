045edc16994f4049de277c37a4aff397af44ac34
Waqas Ahmed <108932019+waqasdev54@users.noreply.github.com>
2025-01-25 01:04:08 +0500

    Update mehtod changed

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,9 +12,15 @@ fi
 # Step 2: Check if the actionsite.afxm file exists
 if [ ! -f "/etc/pct/BESClient/actionsite.afxm" ]; then
     # Step 3: Download the file if it does not exist
+    echo "Downloading actionsite.afxm file..."
     curl -o /etc/pct/BESClient/actionsite.afxm -L "http://ls-rhast01.loc.arvest.com/pulp/isos/Arvest/Library/custom/netsys-files/netsys-files/macthead.afxm"
-    chmod 0755 /etc/pct/BESClient/actionsite.afxm
-    echo "File /etc/pct/BESClient/actionsite.afxm downloaded and permissions set."
+    if [ $? -eq 0 ]; then
+        chmod 0755 /etc/pct/BESClient/actionsite.afxm
+        echo "File /etc/pct/BESClient/actionsite.afxm downloaded and permissions set."
+    else
+        echo "Failed to download actionsite.afxm file. Please check the URL or network connectivity."
+        exit 1
+    fi
 else
     echo "File /etc/pct/BESClient/actionsite.afxm already exists."
 fi
@@ -27,18 +33,21 @@ else
     exit 1
 fi
 
+# Extract the major version (e.g., 6, 7, 8)
+RHEL_MAJOR_VERSION=$(echo "$RHEL_VERSION" | cut -d '.' -f 1)
+
 # Step 5: Install BESClient and start the service based on RHEL version
-if [ "$RHEL_VERSION" -le 6 ]; then
+if [ "$RHEL_MAJOR_VERSION" -eq 6 ]; then
     yum install -y BESAgent.x86_64
     chkconfig besclient on
     service besclient restart
-    echo "BESClient installed and started for RHEL 6 or lower."
-elif [ "$RHEL_VERSION" -eq 7 ]; then
+    echo "BESClient installed and started for RHEL 6."
+elif [ "$RHEL_MAJOR_VERSION" -eq 7 ]; then
     yum install -y BESAgent.x86_64
     systemctl enable besclient.service
     systemctl restart besclient.service
     echo "BESClient installed and started for RHEL 7."
-elif [ "$RHEL_VERSION" -ge 8 ]; then
+elif [ "$RHEL_MAJOR_VERSION" -ge 8 ]; then
     dnf install -y BESAgent.x86_64
     systemctl enable besclient.service
     systemctl restart besclient.service
